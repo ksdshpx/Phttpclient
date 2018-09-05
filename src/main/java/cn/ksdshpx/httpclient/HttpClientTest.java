419e61de1c15a0b036e1689115340e791d9a3b48
ksdshpx <southeast_px@163.com>
2018-09-06 00:14:52 +0800

    HttpClient爬取网站图片以及代理IP的使用！

--- src/main/java/cn/ksdshpx/httpclient/HttpClientTest.java
+++ src/main/java/cn/ksdshpx/httpclient/HttpClientTest.java
@@ -1,6 +1,9 @@
 package cn.ksdshpx.httpclient;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -8,7 +11,9 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Create with IntelliJ IDEA
@@ -22,6 +27,7 @@ public class HttpClientTest {
     public void test() {
         CloseableHttpClient httpClient = null;
         HttpGet httpGet = null;
+        CloseableHttpResponse httpResponse = null;
         try {
             //获取HttpClient客户端
             httpClient = HttpClients.createDefault();
@@ -29,7 +35,7 @@ public class HttpClientTest {
             httpGet = new HttpGet("https://www.tuicool.com");
             httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0)");
             //执行
-            CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+            httpResponse = httpClient.execute(httpGet);
             System.out.println("StatusCode:" + httpResponse.getStatusLine().getStatusCode());
             //返回HttpEntity
             HttpEntity httpEntity = httpResponse.getEntity();
@@ -37,6 +43,105 @@ public class HttpClientTest {
             System.out.println(EntityUtils.toString(httpEntity, "UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if(httpResponse != null){
+                try {
+                    httpResponse.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(httpClient != null){
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testImage() {
+        CloseableHttpClient httpClient = null;
+        HttpGet httpGet = null;
+        CloseableHttpResponse httpResponse = null;
+        try {
+            //获取HttpClient客户端
+            httpClient = HttpClients.createDefault();
+            //创建HttpGet
+            httpGet = new HttpGet("http://www.java1234.com/gg/linux.gif");
+            httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0)");
+            //执行
+            httpResponse = httpClient.execute(httpGet);
+            System.out.println("StatusCode:" + httpResponse.getStatusLine().getStatusCode());
+            //返回HttpEntity
+            HttpEntity httpEntity = httpResponse.getEntity();
+            if(httpEntity != null){
+                System.out.println(httpEntity.getContentType().getName() + ":" + httpEntity.getContentType().getValue());
+                InputStream inputStream = httpEntity.getContent();
+                FileUtils.copyToFile(inputStream,new File("D:\\linux.gif"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(httpResponse != null){
+                try {
+                    httpResponse.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(httpClient != null){
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testProxyIP() {
+        CloseableHttpClient httpClient = null;
+        HttpGet httpGet = null;
+        CloseableHttpResponse httpResponse = null;
+        HttpHost httpHost = null;
+        RequestConfig config = null;
+        try {
+            //获取HttpClient客户端
+            httpClient = HttpClients.createDefault();
+            //创建HttpGet
+            httpGet = new HttpGet("http://www.csdn.com");
+            httpHost = new HttpHost("61.135.217.7",80);
+            config = RequestConfig.custom().setProxy(httpHost).build();
+            httpGet.setConfig(config);
+            httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0)");
+            //执行
+            httpResponse = httpClient.execute(httpGet);
+            System.out.println("StatusCode:" + httpResponse.getStatusLine().getStatusCode());
+            //返回HttpEntity
+            HttpEntity httpEntity = httpResponse.getEntity();
+            System.out.println(httpEntity.getContentType().getName() + ":" + httpEntity.getContentType().getValue());
+            System.out.println(EntityUtils.toString(httpEntity, "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(httpResponse != null){
+                try {
+                    httpResponse.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(httpClient != null){
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
