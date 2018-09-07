b7405462b6aa7eb5782a8e8c5274477c0594f73d
ksdshpx <southeast_px@163.com>
2018-09-07 12:18:38 +0800

    Ehcache缓存框架入门！

---
+++ src/main/java/cn/ksdshpx/ehcache/EhcacheTest.java
@@ -0,0 +1,25 @@
+package cn.ksdshpx.ehcache;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import org.junit.Test;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/9/7
+ * Time: 11:25
+ * Description:Ehcache缓存框架
+ */
+public class EhcacheTest {
+    @Test
+    public void test(){
+        CacheManager cacheManager = CacheManager.create(this.getClass().getClassLoader().getResourceAsStream("ehcache.xml"));
+        Cache cache = cacheManager.getCache("User");
+        Element element = new Element("java","itcast");
+        cache.put(element);
+        Element javaEle = cache.get("java");
+        System.out.println(javaEle.getObjectValue());
+    }
+}
